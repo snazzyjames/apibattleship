d888eaaed4a094d69d4b628f781edad36cdcd702
James <snazzyjames@gmail.com>
2021-03-26 00:24:51 -0500

    Re-add json identifiers

--- models/ship.go
+++ models/ship.go
@@ -1,7 +1,7 @@
 package models
 
 type Ship struct {
-	Name      string
-	Mask      byte
-	HitPoints int
+	Name      string `json:"name"`
+	Mask      byte   `json:"mask"`
+	HitPoints int    `json:"hitpoints"`
 }
